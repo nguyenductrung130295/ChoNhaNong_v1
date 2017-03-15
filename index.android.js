0f039789f4310577f10cfe6586ee873b24a818fc
Trung <trungnguyencntt1302@gmail.com>
2017-03-15 13:24:42 +0700

    15-3

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import {AppRegistry} from 'react-native';
+//import Login from './src/comp/Login';
+//import Register from './src/comp/Register';
+//import FirstDisplay from './src/comp/FirstDisplay';
+import HomeGuest from './src/comp/HomeGuest';
+AppRegistry.registerComponent('ChoNhaNong_v1', () => HomeGuest);
